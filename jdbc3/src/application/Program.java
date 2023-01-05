3da56268c91dae5355c8fe30db8e5f84b16cbcce
Victor Rodrigo Freitas <victorrodrigofreitas@gmail.com>
2023-01-05 13:06:20 -0300

    transacoes

--- jdbc3/src/application/Program.java
+++ jdbc3/src/application/Program.java
@@ -21,21 +21,24 @@ public class Program {
 			conn = DB.getConnection();
 
 			// EXAMPLE 1:
+			/*
 			st = conn.prepareStatement(
 					"INSERT INTO seller "
 					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
-					+ "VALUES " + "(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+					+ "VALUES " 
+					+ "(?, ?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
 
 			st.setString(1, "Carl Purple");
 			st.setString(2, "carl@gmail.com");
 			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
 			st.setDouble(4, 3000.0);
 			st.setInt(5, 4);
-
+			 */
 			// EXAMPLE 2:
-			// st = conn.prepareStatement(
-			// "insert into department (Name) values ('D1'),('D2')",
-			// Statement.RETURN_GENERATED_KEYS);
+			st = conn.prepareStatement(
+					"insert into department (Name) values ('D1'),('D2')",
+					Statement.RETURN_GENERATED_KEYS);
 
 			int rowsAffected = st.executeUpdate();
 
@@ -48,11 +51,14 @@ public class Program {
 			} else {
 				System.out.println("No rows affected!");
 			}
-		} catch (SQLException e) {
+		}
+		catch (SQLException e) {
 			e.printStackTrace();
-		} catch (ParseException e) {
+		}/*
+		catch (ParseException e) {
 			e.printStackTrace();
-		} finally {
+		}*/
+		finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
 		}
